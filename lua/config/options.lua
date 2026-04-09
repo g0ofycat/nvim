d63aa09fb6aea09e4f2d5d2d426d1a22d9226c37
g0ofycat <g0ofycatbusiness@gmail.com>
2026-04-08 20:47:48 -0400

    list

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,8 +8,16 @@ vim.opt.termguicolors = true
 vim.opt.tabstop = 4
 vim.opt.showtabline = 2
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 vim.g.autoformat = false
+
+vim.opt.list = true
+vim.opt.listchars:append({
+    space = '·',
+    tab = '→ ',
+    trail = '•',
+    nbsp = '␣',
+})
 
 --=======================
 -- // SWAP
