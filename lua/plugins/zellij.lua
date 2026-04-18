d98b33df6107707638bf32df4836830e5ef5cd61
g0ofycat <g0ofycatbusiness@gmail.com>
2026-04-18 14:21:52 -0400

    zellij

---
+++ lua/plugins/zellij.lua
@@ -0,0 +1,10 @@
+--=======================
+-- // RETURN
+--=======================
+
+return {
+	"hiasr/vim-zellij-navigator.nvim",
+	config = function()
+		require("vim-zellij-navigator").setup()
+	end,
+}
