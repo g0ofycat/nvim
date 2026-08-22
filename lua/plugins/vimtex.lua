b3c3abd0f327ab8f0b915df097f57ea20976dc5d
g0ofycat <clintonusa5870@gmail.com>
2026-08-21 20:21:59 -0400

    vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,7 +6,6 @@ return {
 	"lervag/vimtex",
 	lazy = false,
 	init = function()
-		vim.g.vimtex_view_general_viewer = "SumatraPDF"
-		vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+		vim.g.vimtex_view_general_viewer = "zathura"
 	end
 }
